59b75242e29ec915950c40409d08a99729225f52
Valero05 <b.valero.r05@hotmail.com>
2023-06-03 21:38:52 -0400

    cambio en views api

--- littlelemonproject/restaurant/views.py
+++ littlelemonproject/restaurant/views.py
@@ -2,10 +2,11 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = ['IsAuthenticated']
+    permission_classes = [IsAuthenticated]
